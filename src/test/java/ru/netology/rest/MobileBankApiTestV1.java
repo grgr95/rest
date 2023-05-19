9c22c33fb479746cffd0df57e6984ca8eb2f4e70
grgr_smrkv <grgrsmrkv@gmail.com>
2023-05-19 11:57:40 +0300

    crash RUR

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,21 +1,38 @@
 package ru.netology.rest;
 
 import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+import io.restassured.http.ContentType;
+
 
 class MobileBankApiTestV1 {
+    private String matcher;
+    private Object operand;
+
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-      .then()
-          .statusCode(200);
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void shouldReturnDemoAccountsBody() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .body(matcher:"[0].currency", equalTo(operand:"RUR"));
     }
 }
